08cd3d5fb9b84bee0f29a9e457cca58d75d7b971
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-03-28 10:19:20 -0600

    testing

--- EndoSfMLearner/run_inference.py
+++ EndoSfMLearner/run_inference.py
@@ -76,7 +76,7 @@ def main():
         if args.output_disp:
             disp = (255*tensor2array(output, max_value=None, colormap='bone')).astype(np.uint8)
             print(disp.shape)
-            print(np.transpose(disp, (1, 2, 0).shape)
+            print(np.transpose(disp, (1, 2, 0).shape))
             #disp = np.delete(disp, 3, 2)
             #imsave(output_dir/'{}_disp{}'.format(file_name, ".png"), np.transpose(disp, (1, 2, 0)))
             #print(disp.shape)
@@ -85,7 +85,7 @@ def main():
             depth = 1/output
             depth = (255*tensor2array(depth, max_value=10, colormap='rainbow')).astype(np.uint8)
             print(depth.shape)
-            print(np.transpose(depth, (1, 2, 0).shape)
+            print(np.transpose(depth, (1, 2, 0).shape))
             #depth = np.delete(depth, 3, 2)
             #imsave(output_dir/'{}_depth{}'.format(file_name, ".png"), np.transpose(depth, (1, 2, 0)))
             #print(depth.shape)
